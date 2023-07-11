c3725b7b5c6ee85132bd8db09f9b0aeb813dc58d
pilu9147 <priyankabhesaniya617@gmail.com>
2023-07-11 12:55:35 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+
+const App =   ()=>{
+  let [text,uptext] = useState('')
+  let dis = (e)=>{
+  uptext(e.target.value)
+  }
   return (
     <div>
-          <p>Enter your name:</p>
+        <p>Enter your name:</p>
         <input type="text" onChange={dis} value={text}/>
         <p>Hello { text}!</p>
     </div>
